52482801ba0b118d6864e39a116a4f760c16df40
David Merfield <dmerfield@gmail.com>
2018-07-26 19:47:34 -0400

    updated node sample app

--- app/index.js
+++ app/index.js
@@ -1,38 +1,34 @@
 var express = require("express");
 var app = new express();
+var TIMEOUT = 10 * 1000; // 10s
 var port = process.env.APP_PORT;
 var build = Date.now();
-var gracefullyExiting = false;
-
-app.use(function(req, res, next) {
-  if (!gracefullyExiting) {
-    return next();
-  }
-  res.setHeader("Connection", "close");
-  return res.send(502, "Server is in the process of restarting.");
-});
 
 app.get("/", function(req, res) {
   res.send(port + " " + build + " " + Date.now());
 });
 
-require('./server-sent-event-middleware')(app, build, port);
+require("./server-sent-event-middleware")(app, build, port);
 
-var liveapp = app.listen(port, function() {
+app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
+// Calling server.close allows us to refuse new requests while
+// continuing to server existing requests. We could just let
+// the process die but this might mean half-finished requests.
 process.on("SIGTERM", function() {
-  gracefullyExiting = true;
   console.log("Received kill signal (SIGTERM), shutting down");
+
   setTimeout(function() {
     console.error(
       "Could not close connections in time, forcefully shutting down"
     );
-    return process.exit(1);
-  }, 10 * 1000);
-  return liveapp.close(function() {
+    process.exit(1);
+  }, TIMEOUT);
+
+  app.close(function() {
     console.info("Closed out remaining connections.");
-    return process.exit();
+    process.exit();
   });
 });
